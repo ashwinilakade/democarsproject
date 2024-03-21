64e5325e79905a110853515b5d626012f6a1d6eb
ashwinilakade <ashwinibhivamote123@gmail.com>
2024-03-21 11:57:47 +0530

    brand changes

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
 export function Home() {
+  const brands = [
+    {name: 'Tesla', image: 'assets/tesla.png'},
+    {name: 'Toyota', image: 'assets/toyota.png'},
+    {name: 'Honda', image: 'assets/honda.png'},
+    {name: 'Ford', image: 'assets/ford.png'},
+  ];
   return (
     <div className="container my-5">
-      <h2 className="text-center mb-3"> Home Page </h2>
+      <div className="row">
+        {brands?.map((item, index) => (
+          <div className="col" key={index}>
+            <img
+              src={item.image}
+              alt={item.name}
+              style={{width: '100%', height: 'auto', border: '1px solid black'}}
+            />
+            <span style={{fontWeight: 'bold'}}>{item.name}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -22,11 +22,6 @@ export function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link text-dark" aria-current="page" to="/">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
               <Link className="nav-link text-dark" to="/products">
                 Products
               </Link>
